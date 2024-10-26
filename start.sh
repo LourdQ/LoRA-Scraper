f6c2a5a35d72bf983480eca976000082559fa50a
LourdQ <Ai_ArtGuy@icloud.com>
2024-10-25 22:41:34 -0400

    Add PORT debugging and ensure gunicorn usage

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo "Current PORT setting: $PORT"
 python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
+echo "Starting gunicorn..."
 gunicorn --bind 0.0.0.0:$PORT backend.api_server:app
